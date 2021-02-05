fb5c0886becd2e303f387474d422f673e60f0e5b
QDenka <a@bigf.at>
2021-02-06 00:58:42 +0300

    init

---
+++ src/Type/ThreatEntryType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Saippuakauppias\SafeBrowsing\Type;
+
+class ThreatEntryType
+{
+    const THREAT_ENTRY_TYPE_UNSPECIFIED = 'THREAT_ENTRY_TYPE_UNSPECIFIED';
+    const URL = 'URL';
+    const EXECUTABLE = 'EXECUTABLE';
+}
